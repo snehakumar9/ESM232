fb172f7e0169e6d0a0f28815e4d223e17924daf7
Sneha Kumar <snehakumar@ESM.UCSB.EDU>
2026-01-19 00:46:12 -0800

    Updates & simplification of function

--- R/HW3.R
+++ R/HW3.R
@@ -1,25 +1,14 @@
-#Function HW is used to calculate the yield anomaly in ton/acre for almond crops 
-#Inputs are the following: 
-#min_temp- a vector containing only the monthly average daily minimum temperature (C) for the month of Feb for each study year
-#av_percip- a vector containing only the monthly average percipitation (mm) for the month of January for each study year
-#The output of the function is a summary table containing the min, max, and mean yield anomaly over all study years
+# Function Name: Yield Anomaly for Almond Crops
+# This function is used to calculate the anomalies in yield (ton/acre) for almond crops
+# based on average precipitation in January and the average minimum temperature in February
+# for the year in which yield anomaly is calculated
 
-#Call the function
-HW3 <- function(min_temp, av_percip) {
-  
-  #Calculate the yield anomaly using the temp and precipitation vectors
-  yield_anomaly<- -0.015*min_temp-0.0046*(min_temp^2)-0.07*av_percip+0.0043*(av_percip^2)+.28
-  yield_anomaly<- yield_anomaly %>%
-    rename(yearly_yield_anomaly= mean_min_temp_feb)
-  
-  #Create a summary Table for the yield anomaly min, max, and mean
-  yield_anomaly_summary<- yield_anomaly %>%
-    summarise(
-      Mean = mean(yearly_yield_anomaly, na.rm = TRUE),
-      Minimum = min(yearly_yield_anomaly, na.rm = TRUE),
-      Maximum = max(yearly_yield_anomaly, na.rm = TRUE)
-    )
-  
-  #Return the yield anomaly summary table
-  return(yield_anomaly_summary)
+# Inputs are the following: 
+# min_temp - the average minimum temperature (C) for the month of Feb for each study year
+# avg_precip - the average precipitation (mm) for the month of January for each study year
+# The output of the function is a summary table containing the min, max, and mean yield anomaly over all study years
+
+#Create Function
+yield_anomaly <- function(min_temp, av_precip) {
+  return(-0.015*min_temp - (0.0046*(min_temp)^2) - (0.07*av_precip) + (0.0043*(av_precip)^2) + 0.28)
 }
